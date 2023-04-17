ef205e92b607a68dc63ebea3b281b6b7b41e6884
Lovi Stark <lovishb41@gmail.com>
2023-04-17 23:42:50 +0530

    animated_button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:eight_hour_tutorial/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +39,8 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const Text(
-              "Welcome",
+            Text(
+              "Welcome $name",
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             const SizedBox(
@@ -48,6 +56,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter User name",
                       labelText: "UserName",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     // obscureText to hide text
@@ -60,20 +72,58 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // print("Login Pressed");
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     // print("Login Pressed");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //     shadowColor: Colors.yellow,
+                  //     // iconColor: Colors.pink,
+                  //     foregroundColor: Colors.tealAccent,
+                  //     // backgroundColor: Colors.black,
+                  //     surfaceTintColor: Colors.red,
+                  //     minimumSize: Size(120, 40),
+                  //   ),
+                  //   child: const Text("Login"),
+                  // ),
+
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(
+                        Duration(seconds: 1),
+                      );
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    style: TextButton.styleFrom(
-                      shadowColor: Colors.yellow,
-                      // iconColor: Colors.pink,
-                      foregroundColor: Colors.tealAccent,
-                      // backgroundColor: Colors.black,
-                      surfaceTintColor: Colors.red,
-                      minimumSize: Size(120, 40),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape:
+                        //     changeButton ? BoxShape.circle : BoxShape.rectangle,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                      child: changeButton
+                          ? const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
                     ),
-                    child: const Text("Login"),
                   ),
                 ],
               ),
