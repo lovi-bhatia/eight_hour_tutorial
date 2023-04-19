e5359f36476ffaecf8c6cda2bead67151f08fff3
Lovi Stark <lovishb41@gmail.com>
2023-04-19 18:02:12 +0530

    catalog_json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,44 @@
 import 'package:eight_hour_tutorial/models/catalog.dart';
 import 'package:flutter/material.dart';
 import 'package:eight_hour_tutorial/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Lovi";
 
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    // print(decodeData);
+
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(
+        CatalogModel.items.length, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
