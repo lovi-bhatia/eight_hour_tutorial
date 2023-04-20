ea4cfee8de2ce88197b3b7dcf8035c3e0d052448
Lovi Stark <lovishb41@gmail.com>
2023-04-20 15:39:22 +0530

    GridView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -60,11 +60,47 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items?.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
+            // ? ListView.builder(
+            //     itemCount: CatalogModel.items?.length,
+            //     itemBuilder: (context, index) => ItemWidget(
+            //       item: CatalogModel.items![index],
+            //     ),
+            //   )
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
                 ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items?[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item!.name,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      child: Image.network(
+                        item.image,
+                        fit: BoxFit.contain,
+                      ),
+                      footer: Text(item.price.toString()),
+                    ),
+                  );
+                },
+                itemCount: CatalogModel.items?.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
